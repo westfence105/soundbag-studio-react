f9ce1839be97a47b3fed5546a95e4bb724cae2e9
Togo Nishigaki <westfence95@gmail.com>
2018-05-20 05:05:55 +0000

    Implement main-navigation menu

--- src/pages/MainTitlePage.js
+++ src/pages/MainTitlePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import "../App.css";
 import "./MainTitlePage.css";
@@ -10,6 +11,13 @@ class MainTitlePage extends Component {
                 <h1 className="main-title">soundbag studio</h1>
                 <hr className="star" />
                 <div className="caption">Software Engineer & Sound Producer</div>
+                <ul className="main-menu">
+                    <li id="main-menu-about"   ><Link to="/about"   >ABOUT</Link></li>
+                    <li id="main-menu-profile" ><Link to="/profile" >PROFILE</Link></li>
+                    <li id="main-menu-engineer"><Link to="/engineer">ENGINEER</Link></li>
+                    <li id="main-menu-music"   ><Link to="/music"   >MUSIC</Link></li>
+                    <li id="main-menu-contact" ><Link to="/contact" >CONTACT</Link></li>
+                </ul>
             </div>
         );
     }
